b259fb91837fc511e893cc20013bf94c12336f1e
dev <redherring2141@kaist.ac.kr>
2020-04-03 22:00:09 +0900

    First version, simple PID control implementation.

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,7 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
+  pid.Init(0.15, 0.0, 2.5);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -56,17 +57,20 @@ int main() {
           double cte = std::stod(j[1]["cte"].get<string>());
           double speed = std::stod(j[1]["speed"].get<string>());
           double angle = std::stod(j[1]["steering_angle"].get<string>());
-          double steer_value;
+          double steer_value = 0.0;
           /**
            * TODO: Calculate steering value here, remember the steering value is
            *   [-1, 1].
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
-          
+
+          pid.UpdateError(cte);
+          steer_value = - pid.TotalError();
+                    
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
-                    << std::endl;
+                    << "Total Error: "<< pid.TotalError() << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
